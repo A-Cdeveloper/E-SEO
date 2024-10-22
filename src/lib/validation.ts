2f8f400abe1389a635a030ae3ba872004a337e88
Aleksandar <aleksandar@e-vlasotince.info>
2024-10-22 16:02:17 +0200

    fix phone validation

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -4,7 +4,14 @@ import { z } from "zod";
 export const formSchema = z.object({
   fullname: z.string().trim().min(1, { message: "Name is required" }),
   email: z.string().trim().email({ message: "Invalid email address" }),
-  phone: z.string().regex(/^\+\d{1,3}\d{7,}$/, {
-    message: "Phone number must start with a '+' followed by the country code",
-  }),
+  phone: z
+    .string()
+    .trim()
+    .min(1, { message: "Phone is required" })
+    .or(
+      z.string().regex(/^\+\d{1,3}\d{7,}$/, {
+        message:
+          "Phone number must start with a '+' followed by the country code",
+      })
+    ),
 });
